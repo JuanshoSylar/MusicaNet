ed0330e45c9c6a6c417f2851b6e290d00be21631
brahmaxd <abraham.antil.ra@gmail.com>
2013-10-26 00:43:51 -0300

    FrCancion
    coodigo no diseño :D

--- ProyectoMusica/frCanciones.aspx.cs
+++ ProyectoMusica/frCanciones.aspx.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
 
 public partial class frCanciones : System.Web.UI.Page
 {
+    DataTable dt;
     protected void Page_Load(object sender, EventArgs e)
     {
+        leer();
+    }
+    protected void btBuscar_Click(object sender, EventArgs e)
+    {
+        leer();
+    }
 
+    public void leer()
+    {
+
+        dt = BibliotecaMusica.DAO.DAOCancion.sqlLeerTodas(txtNombreCan.Text);
+        grDatosCanciones.DataSource = dt;
+        grDatosCanciones.DataBind();
+
+
+    }
+    protected void grDatosCanciones_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataRow row = dt.Rows[grDatosCanciones.SelectedIndex];
+        // txtMsg.Text = grDatosCanciones.SelectedIndex.ToString() + row["id"].ToString();
+        Session["id"] = row["id"].ToString();
+        Response.Redirect("frCancion.aspx");//llena los datos de cancionadmin
     }
 }
