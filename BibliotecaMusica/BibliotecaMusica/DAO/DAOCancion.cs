3b11e02ec7537278a8fc4d4b386383995a30c872
JuanshoSylar <moonmonster@hotmail.cl>
2013-10-26 17:34:44 -0300

    DAO Canción Actualizado

--- BibliotecaMusica/BibliotecaMusica/DAO/DAOCancion.cs
+++ BibliotecaMusica/BibliotecaMusica/DAO/DAOCancion.cs
@@ -16,11 +16,8 @@ namespace BibliotecaMusica.DAO
         {
             DataTable dt = new DataTable();
             BD bd = Conexion.BD.getInstance();
-            // dt= bd.sqlSelect("select * from cancion");
-            String sql = "select * from cancion where 1=1 ";
-            //if (id.ToString().Trim() != "") sql += String.Format("and id like '%{0}'",id);
-            if (nombre.Trim() != "") sql += String.Format(" and nombre like '%{0}'", nombre);
-
+            String sql = "Select * from cancion where 1=1";
+            if (nombre.Trim() != "") sql += String.Format(" and nombre like '%{0}%'", nombre);
             dt = bd.sqlSelect(20, sql);
             return dt;
         }
@@ -28,15 +25,17 @@ namespace BibliotecaMusica.DAO
 
         public static Cancion sqlLeer(Cancion cancion)
         {
-            String sql = "select * cancion from where id = '" + cancion.Id + "'";
+            String sql = "Select * from cancion where id = '" + cancion.Id + "'";
             BD bd = Conexion.BD.getInstance();
+            bd.sqlEjecutar(sql);
             return cancion;
         }
 
         public static Boolean sqlInsert(Cancion cancion)
         {
-            String sql = "insert into cancion (id, nombre) values ('" + cancion.Id + "','" + cancion.Nombre + "')";
+            String sql = "Insert into cancion (id, nombre, idDisco) values ('" + cancion.Id + "','" + cancion.Nombre + "', '" + cancion.IdDisco + "')";
             BD bd = Conexion.BD.getInstance();
+            bd.sqlEjecutar(sql);
             return true;
         }
 
@@ -44,13 +43,15 @@ namespace BibliotecaMusica.DAO
         {
             String sql = "Delete from cancion where id = '" + cancion.Id + "'";
             BD bd = Conexion.BD.getInstance();
+            bd.sqlEjecutar(sql);
             return true;
 
         }
         public static Boolean sqlUpdate(Cancion cancion)
         {
-            string sql = "update cancion set nombre '" + cancion.Nombre + "' where id = '" + cancion.Id + "'";
+            string sql = "Update cancion set nombre '" + cancion.Nombre + "', idDisco = '" + cancion.IdDisco + "' where id = '" + cancion.Id + "'";
             BD bd = Conexion.BD.getInstance();
+            bd.sqlEjecutar(sql);
             return true;
 
         }
